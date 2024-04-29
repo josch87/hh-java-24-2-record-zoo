192e4b397c22853d2a7b01168b5f7d5f5e831192
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-04-29 12:34:27 +0200

    Add account management and money transfer functions
    Implemented key functionality for the BankService class. It now includes methods to manage account sets and a mechanism for making money transfers between accounts. This ensures a higher level of control and interaction within the bank service system.

--- src/main/java/com/aljoschazoeller/java/bankservice/BankService.java
+++ src/main/java/com/aljoschazoeller/java/bankservice/BankService.java
@@ -1,13 +1,44 @@
 package com.aljoschazoeller.java.bankservice;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
 public class BankService {
     Set<Account> accounts = new HashSet<>();
 
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+
     public String openAccount(Client customer) {
         Account newAccount = new Account(customer);
+        accounts.add(newAccount);
         return newAccount.getAccountNumber();
+    }
+
+    public void transferMoney(String sourceAccountNumber, String targetAccountNumber, BigDecimal amount) {
+        Account sourceAccount = null;
+        Account targetAccount = null;
+
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(sourceAccountNumber)) {
+                sourceAccount = account;
+            }
+        }
+
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(targetAccountNumber)) {
+                targetAccount = account;
+            }
+        }
+        if (sourceAccount != null && targetAccount != null) {
+            sourceAccount.withdrawMoney(amount);
+            targetAccount.depositMoney(amount);
+        }
     }
 }
